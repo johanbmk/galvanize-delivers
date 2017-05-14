cb044c4b1a48f65fc93f4eca88525b38a77d66fc
Johan Brattemark <johan.brattemark@gmail.com>
2017-05-14 10:43:45 -0700

    Add jquery for order button

---
+++ js/order.js
@@ -0,0 +1,45 @@
+let subtotal = 0;
+
+// "Add to order" event listeners and handlers
+for (var id of ["burger", "ribs", "pizza", "ice_cream"]) {
+  $(`#order-${id}`).click((event) => {
+    event.preventDefault(); // so <a href="#"> does not scroll
+
+    // Get choice name and price
+    let choiceName = event.target.parentElement.parentElement.children[1].children[0].innerText;
+    let choicePriceText = event.target.parentElement.parentElement.children[1].children[1].innerText;
+
+    // Add to the table of orders
+    var tr = $('<tr>');
+    tr.append($('<td>').text(choiceName));
+    tr.append($('<td class="right-align">').text(choicePriceText));
+    $('#table-of-orders tbody').append(tr);
+
+    // Update subtotal
+    let price = Number(choicePriceText.replace(/\$(\d+\.\d+)/, '$1'));
+    subtotal += price;
+    let subtotalText = '$' + subtotal.toFixed(2);
+    $('#subtotal').text(subtotalText);
+
+    // Update tax
+    let tax = subtotal * 0.08;
+    let taxText = '$' + tax.toFixed(2);
+    $('#tax').text(taxText);
+
+    // Update total
+    let total = subtotal + tax;
+    let totalText = '$' + total.toFixed(2);
+    $('#total').text(totalText);
+  });
+}
+
+
+// "Place order" event listener and handler
+$('#order-button').click((event) => {
+  event.preventDefault(); // so <a href="#"> does not scroll
+  if (subtotal === 0) {
+    Materialize.toast('No order was placed. There is currently nothing in the order list.', 4000);
+  } else {
+    Materialize.toast('Your order has been placed. Bon appetit!', 4000);
+  }
+});
